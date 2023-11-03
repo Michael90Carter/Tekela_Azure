5b3605bb71affbc9b1fcb33d21bc36cc9d003343
Michael <Michael@DESKTOP-6395OH7>
2023-11-03 15:45:13 +0300

    add migration Context

---
+++ Tekela.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tekela.Services.ProductAPI.Models;
+
+namespace Tekela.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
